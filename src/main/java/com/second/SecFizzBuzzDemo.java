5799ed45fd91581dc5635ff01572c593c98bdafb
qianwu2 <qianwu2@ap.xom.com>
2020-01-10 14:17:11 +0800

    secfizzbuzz [qian] should return FizzBuzz when fizzBuzz given 60 and add enum ZzType

--- src/main/java/com/second/SecFizzBuzzDemo.java
+++ src/main/java/com/second/SecFizzBuzzDemo.java
@@ -1,17 +1,25 @@
 package com.second;
 
+import static com.second.ZzType.BUZZ;
+import static com.second.ZzType.FIZZ;
+import static com.second.ZzType.WHIZZ;
+
 public class SecFizzBuzzDemo {
 
   public String fizzBuzz(int number) {
-    if (number % 3 == 0) {
-      return "Fizz";
-    }
-    if (number % 5 == 0) {
-      return "Buzz";
-    }
-    if (number % 7 == 0) {
-      return "Whizz";
-    }
-    return String.valueOf(number);
+    String returnForSpecialNumbers = getReturnForSpecialNumber(number, FIZZ) +
+        getReturnForSpecialNumber(number, BUZZ) +
+        getReturnForSpecialNumber(number, WHIZZ);
+    return returnForSpecialNumbers.isEmpty() ? String.valueOf(number) : returnForSpecialNumbers;
   }
+
+  private String getReturnForSpecialNumber(int number, ZzType zzType) {
+    if (number % zzType.getNumber() == 0) {
+      return zzType.getString();
+
+    }
+    return "";
+  }
+
+
 }
--- src/test/java/com/second/SecFizzBuzzDemoTest.java
+++ src/test/java/com/second/SecFizzBuzzDemoTest.java
@@ -7,27 +7,34 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SecFizzBuzzDemoTest {
 
   @Test
-  void should_return_1_when_fizzbuzz_given_1() {
+  void should_return_1_when_fizzBuzz_given_1() {
     String actualResult = new SecFizzBuzzDemo().fizzBuzz(1);
     assertEquals("1", actualResult);
   }
 
   @Test
-  void should_return_Fizz_when_fizzbuzz_given_6() {
+  void should_return_Fizz_when_fizzBuzz_given_6() {
     String actualResult = new SecFizzBuzzDemo().fizzBuzz(6);
     assertEquals("Fizz", actualResult);
 
   }
 
   @Test
-  void should_return_Buzz_when_fizzbuzz_given_10() {
+  void should_return_Buzz_when_fizzBuzz_given_10() {
     String actualResult = new SecFizzBuzzDemo().fizzBuzz(10);
     assertEquals("Buzz", actualResult);
   }
 
   @Test
-  void should_return_Whizz_when_fizzbuzz_given_14() {
+  void should_return_Whizz_when_fizzBuzz_given_14() {
     String actualResult = new SecFizzBuzzDemo().fizzBuzz(14);
     assertEquals("Whizz", actualResult);
+  }
+
+  @Test
+  void should_return_FizzBuzz_when_fizzBuzz_given_60() {
+    String actualResult = new SecFizzBuzzDemo().fizzBuzz(60);
+    assertEquals("FizzBuzz", actualResult);
+
   }
 }
