7abaa68ff2fec97b22b2306b512f1ed29a95eba6
qianwu2 <qianwu2@ap.xom.com>
2020-01-10 12:10:01 +0800

    fizzbuzz [qian] do some refactor

--- src/main/java/com/FizzBuzzDemo.java
+++ src/main/java/com/FizzBuzzDemo.java
@@ -11,24 +11,21 @@ public class FizzBuzzDemo {
       return getReturnValueForTargetNumber(number, FIZZ) + getReturnValueForTargetNumber(number, WHIZZ);
     }
     if (isContainsTargetNumber(number, BUZZ.getNumber())) {
-      if (!isTheMultipleOfAnyTargetNumber(number, BUZZ.getNumber(), WHIZZ.getNumber())) {
-        return String.valueOf(number);
-      }
-      return getReturnValueForTargetNumber(number, BUZZ) + getReturnValueForTargetNumber(number, WHIZZ);
+      String returnForSpecialNumber = getReturnValueForTargetNumber(number, BUZZ) + getReturnValueForTargetNumber(number, WHIZZ);
+      return returnForSpecialNumber.isEmpty() ? String.valueOf(number) : returnForSpecialNumber;
     }
     if (isContainsTargetNumber(number, FIZZ.getNumber())) {
       return FIZZ.getString();
     }
-    if (isTheMultipleOfAnyTargetNumber(number, FIZZ.getNumber(), BUZZ.getNumber(), WHIZZ.getNumber())) {
-      return getReturnValueForTargetNumber(number, FIZZ) +
-          getReturnValueForTargetNumber(number, BUZZ) +
-          getReturnValueForTargetNumber(number, WHIZZ);
-    }
-    return String.valueOf(number);
+    String returnForSpecialNumbers = getReturnValueForTargetNumber(number, FIZZ) +
+        getReturnValueForTargetNumber(number, BUZZ) +
+        getReturnValueForTargetNumber(number, WHIZZ);
+
+    return returnForSpecialNumbers.isEmpty() ? String.valueOf(number): returnForSpecialNumbers;
   }
 
   private String getReturnValueForTargetNumber(int number, ZzType zzType) {
-    if (isTheMultipleOfTargetNumber(number, zzType.getNumber())) {
+    if (number % zzType.getNumber() == 0) {
       return zzType.getString();
     }
     return "";
@@ -37,18 +34,5 @@ public class FizzBuzzDemo {
 
   private boolean isContainsTargetNumber(int originalNumber, int targetNumber) {
     return String.valueOf(originalNumber).contains(String.valueOf(targetNumber));
-  }
-
-  private boolean isTheMultipleOfTargetNumber(int originalNumber, int targetNumber) {
-    return originalNumber % targetNumber == 0;
-  }
-
-  private boolean isTheMultipleOfAnyTargetNumber(int originalNumber, int... targetNumbers) {
-    for (int targetNumber: targetNumbers) {
-      if (originalNumber % targetNumber == 0) {
-        return true;
-      }
-    }
-    return false;
   }
 }
