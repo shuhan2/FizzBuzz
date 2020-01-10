d0e710e8eed69e141c077fd042e5d77cff533fb4
qianwu2 <qianwu2@ap.xom.com>
2020-01-10 15:14:28 +0800

    secfizzbuzz [qian] return Buzz  when fizzBuzz given multiple of 5

--- src/test/java/com/third/ThirdFizzBuzzDemoTest.java
+++ src/test/java/com/third/ThirdFizzBuzzDemoTest.java
@@ -17,4 +17,10 @@ class ThirdFizzBuzzDemoTest {
     String actualResult = new ThirdFizzBuzzDemo().fizzBuzz(6);
     assertEquals("Fizz", actualResult);
   }
+
+  @Test
+  void should_return_Buzz_when_fizzBuzz_given_10() {
+    String actualResult = new ThirdFizzBuzzDemo().fizzBuzz(10);
+    assertEquals("Buzz", actualResult);
+  }
 }
