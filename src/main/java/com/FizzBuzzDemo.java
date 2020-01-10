ce4179e3d5cc683d87c1171b57539fe16de428c3
qianwu2 <qianwu2@ap.xom.com>
2020-01-10 10:44:23 +0800

    fizzbuzz [qian] return original number when fizzBuzz if number is 1

---
+++ src/main/java/com/FizzBuzzDemo.java
@@ -0,0 +1,8 @@
+package com;
+
+public class FizzBuzzDemo {
+
+  public String fizzBuzz(int number) {
+    return String.valueOf(number);
+  }
+}
---
+++ src/test/java/com/FizzBuzzDemoTest.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FizzBuzzDemoTest {
+
+  @Test
+  void should_return_1_when_fizz_buzz_given_number_is_1() {
+    String actualResult = new FizzBuzzDemo().fizzBuzz(1);
+    assertEquals("1", actualResult);
+  }
+}
