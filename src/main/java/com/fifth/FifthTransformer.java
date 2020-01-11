3ceb48575fd0e8d0f536f33b404a66175846528e
qianwu2 <qianwu2@ap.xom.com>
2020-01-11 19:37:12 +0800

    fifthfizzbuzz [qian] return 51 when fizzBuzz given 51

--- src/main/java/com/fifth/FifthTransformer.java
+++ src/main/java/com/fifth/FifthTransformer.java
@@ -4,12 +4,15 @@ import static com.fifth.FifthZz.*;
 
 public class FifthTransformer {
 
+  private static final String EMPTY_STRING = "";
+
   public String fizzBuzz(int number) {
     String returnForSpecialNumbers = getReturnForSpecialZz(number, FIZZ) +
         getReturnForSpecialZz(number, BUZZ) +
         getReturnForSpecialZz(number, WHIZZ);
     if (String.valueOf(number).contains("5")) {
-      return returnForSpecialNumbers;
+      returnForSpecialNumbers = returnForSpecialNumbers.replace("Fizz", EMPTY_STRING);
+      return returnForSpecialNumbers.isEmpty() ? String.valueOf(number) : returnForSpecialNumbers;
     }
     if (String.valueOf(number).contains("3")) {
       return "Fizz";
@@ -22,6 +25,6 @@ public class FifthTransformer {
     if (number % zz.getNumber() == 0) {
       return zz.getReturnValue();
     }
-    return "";
+    return EMPTY_STRING;
   }
 }
--- src/test/java/com/fifth/FifthTransformerTest.java
+++ src/test/java/com/fifth/FifthTransformerTest.java
@@ -9,13 +9,14 @@ class FifthTransformerTest {
 
   @ParameterizedTest
   @CsvSource({
-      "'1', 1, 'should return 1 when fizzBuzz given 1'",
-      "'Fizz', 12, 'should return Fizz when fizzBuzz given 12'",
-      "'Buzz', 20, 'should return Buzz when fizzBuzz given 20'",
-      "'Whizz', 28, 'should return Whizz when fizzBuzz given 28'",
-      "'FizzBuzzWhizz', 210, 'should return FizzBuzzWhizz when fizzBuzz given 210'",
-      "'Fizz', 30, 'should return Fizz when fizzBuzz given 30'",
-      "'BuzzWhizz', 35, 'should return Fizz when fizzBuzz given 35'"
+      "'1', 1",
+      "'Fizz', 12",
+      "'Buzz', 20",
+      "'Whizz', 28",
+      "'FizzBuzzWhizz', 210",
+      "'Fizz', 30",
+      "'BuzzWhizz', 35",
+      "'51', 51"
   })
   void should_return_expected_when_fizzBuzz_given_input_number(String output, int input) {
     String actualResult = new FifthTransformer().fizzBuzz(input);
