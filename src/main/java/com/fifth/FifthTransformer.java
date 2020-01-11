bf0765cf859a1ddb835bc33062fe2d1ca22954fa
qianwu2 <qianwu2@ap.xom.com>
2020-01-11 19:23:43 +0800

    fifthfizzbuzz [qian] return Fizz when fizzBuzz given 12

--- src/main/java/com/fifth/FifthTransformer.java
+++ src/main/java/com/fifth/FifthTransformer.java
@@ -3,6 +3,9 @@ package com.fifth;
 public class FifthTransformer {
 
   public String fizzBuzz(int number) {
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
     return String.valueOf(number);
   }
 }
--- src/test/java/com/fifth/FifthTransformerTest.java
+++ src/test/java/com/fifth/FifthTransformerTest.java
@@ -10,7 +10,8 @@ class FifthTransformerTest {
 
   @ParameterizedTest
   @CsvSource({
-      "'1', 1, 'should return 1 when fizzBuzz given 1'"
+      "'1', 1, 'should return 1 when fizzBuzz given 1'",
+      "'Fizz', 12, 'should return Fizz when fizzBuzz given 12'"
   })
   void should_return_expected_when_fizzBuzz_given_input_number(String output, int input) {
     String actualResult = new FifthTransformer().fizzBuzz(input);
