7190c3d9e5de2e56275ec0c149e79dd9f376acff
qianwu2 <qianwu2@ap.xom.com>
2020-01-11 21:28:19 +0800

    sixthfizzbuzz [qian] return Fizz when fizzBuzz given only match multiple 3 rule

--- src/main/java/com/sixth/SixthTransformer.java
+++ src/main/java/com/sixth/SixthTransformer.java
@@ -3,6 +3,9 @@ package com.sixth;
 public class SixthTransformer {
 
   public String fizzBuzz(int number) {
+    if (number % 3 == 0) {
+      return "Fizz";
+    }
     return String.valueOf(number);
   }
 }
--- src/test/java/com/sixth/SixthTransformerTest.java
+++ src/test/java/com/sixth/SixthTransformerTest.java
@@ -1,15 +1,15 @@
 package com.sixth;
 
-import com.fifth.FifthTransformer;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SixthTransformerTest {
   @ParameterizedTest
   @CsvSource({
-      "'1', 1, 'should return original value when fizzBuzz'"
+      "'1', 1, 'should return original value when fizzBuzz given no special value'",
+      "'Fizz', 12, 'should return original value when fizzBuzz given only match multiple 3 rule'"
 
   })
   void should_return_expected_when_fizzBuzz_given_input_number(String output, int input) {
