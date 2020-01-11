e4ee171cc48f5216b3d88d4c8e5360b466bfbd2c
qianwu2 <qianwu2@ap.xom.com>
2020-01-11 19:34:10 +0800

    fifthfizzbuzz [qian] return BuzzWhizz when fizzBuzz given 35

--- src/main/java/com/fifth/FifthTransformer.java
+++ src/main/java/com/fifth/FifthTransformer.java
@@ -5,12 +5,15 @@ import static com.fifth.FifthZz.*;
 public class FifthTransformer {
 
   public String fizzBuzz(int number) {
-    if (String.valueOf(number).contains("3")) {
-      return "Fizz";
-    }
     String returnForSpecialNumbers = getReturnForSpecialZz(number, FIZZ) +
         getReturnForSpecialZz(number, BUZZ) +
         getReturnForSpecialZz(number, WHIZZ);
+    if (String.valueOf(number).contains("5")) {
+      return returnForSpecialNumbers;
+    }
+    if (String.valueOf(number).contains("3")) {
+      return "Fizz";
+    }
 
     return returnForSpecialNumbers.isEmpty() ? String.valueOf(number) : returnForSpecialNumbers;
   }
--- src/test/java/com/fifth/FifthTransformerTest.java
+++ src/test/java/com/fifth/FifthTransformerTest.java
@@ -14,7 +14,8 @@ class FifthTransformerTest {
       "'Buzz', 20, 'should return Buzz when fizzBuzz given 20'",
       "'Whizz', 28, 'should return Whizz when fizzBuzz given 28'",
       "'FizzBuzzWhizz', 210, 'should return FizzBuzzWhizz when fizzBuzz given 210'",
-      "'Fizz', 30, 'should return Fizz when fizzBuzz given 30'"
+      "'Fizz', 30, 'should return Fizz when fizzBuzz given 30'",
+      "'BuzzWhizz', 35, 'should return Fizz when fizzBuzz given 35'"
   })
   void should_return_expected_when_fizzBuzz_given_input_number(String output, int input) {
     String actualResult = new FifthTransformer().fizzBuzz(input);
