d215e55cce4f6ebc4d095c48170699256831fe88
qianwu2 <qianwu2@ap.xom.com>
2020-01-12 12:47:58 +0800

    seventhfizzbuzz [qian] return Whizz when fizzBuzz given number only multiple 7

--- src/main/java/com/seventh/SeventhTransformer.java
+++ src/main/java/com/seventh/SeventhTransformer.java
@@ -6,11 +6,12 @@ public class SeventhTransformer {
     if (number % 3 == 0) {
       return "Fizz";
     }
-
     if (number % 5 == 0) {
       return "Buzz";
     }
-
+    if (number % 7 == 0) {
+      return "Whizz";
+    }
     return String.valueOf(number);
   }
 }
--- src/test/java/com/seventh/SeventhTransformerTest.java
+++ src/test/java/com/seventh/SeventhTransformerTest.java
@@ -11,7 +11,8 @@ class SeventhTransformerTest {
   @CsvSource({
       "'2', 2, 'should return original string value when fizzBuzz given no special number'",
       "'Fizz', 12, 'should return Fizz when fizzBuzz given only multiple 3'",
-      "'Buzz', 20, 'should return Fizz when fizzBuzz given only multiple 5'",
+      "'Buzz', 20, 'should return Buzz when fizzBuzz given only multiple 5'",
+      "'Whizz', 28, 'should return Whizz when fizzBuzz given only multiple 7'",
   })
   void should_return_expect_value_when_fizzBuzz_given_input_number(String output, int input) {
     String actualResult = new SeventhTransformer().fizzBuzz(input);
